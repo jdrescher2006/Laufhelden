1b613025e4135c992649b8352b233ee812ce5eae
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-09-27 07:35:57 +0200

    Create harbour-laufhelden-sv.ts
    Here's Swedish translation!

---
+++ translations/harbour-laufhelden-sv.ts
@@ -0,0 +1,526 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>Licens: GPLv3</translation>
+    </message>
+    <message>
+        <source>Source code:</source>
+        <translation>Källkod: </translation>
+    </message>
+    <message>
+        <source>Date: </source>
+        <translation>Datum: </translation>
+    </message>
+    <message>
+        <source>Feedback, bugs:</source>
+        <translation>Återkoppling, fel:</translation>
+    </message>
+    <message>
+        <source>About Laufhelden</source>
+        <translation>Om Laufhelden</translation>
+    </message>
+    <message>
+        <source>Sport tracker application for Sailfish OS</source>
+        <translation>Spårning av sportaktiviteter för Sailfish OS</translation>
+    </message>
+</context>
+<context>
+    <name>BTConnectPage</name>
+    <message>
+        <source>Scan for Bluetooth devices</source>
+        <translation>Sök efter bluetooth-enheter</translation>
+    </message>
+    <message>
+        <source>Start scanning...</source>
+        <translation>Starta sökning...</translation>
+    </message>
+    <message>
+        <source>Cancel scanning</source>
+        <translation>Avbryt sökning</translation>
+    </message>
+    <message>
+        <source>Current BT device</source>
+        <translation>Aktuell bt-enhet</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Ingen</translation>
+    </message>
+    <message>
+        <source>Heart Rate: </source>
+        <translation>Hjärtfrekvens: </translation>
+    </message>
+    <message>
+        <source> bpm</source>
+        <translation> bpm</translation>
+    </message>
+    <message>
+        <source>Battery Level: </source>
+        <translation>Batterinivå: </translation>
+    </message>
+    <message>
+        <source>Found BT devices (press to connect):</source>
+        <translation>Hittade bt-enheter (tryck för att ansluta):</translation>
+    </message>
+    <message>
+        <source>Heart rate device</source>
+        <translation>Hjärtfrekvensenhet</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Recording</source>
+        <translation>Spelar in</translation>
+    </message>
+    <message>
+        <source>Stopped</source>
+        <translation>Stoppad</translation>
+    </message>
+    <message>
+        <source> bpm</source>
+        <translation> bpm</translation>
+    </message>
+</context>
+<context>
+    <name>DetailedViewPage</name>
+    <message>
+        <source>Starting time:</source>
+        <translation>Starttid:</translation>
+    </message>
+    <message>
+        <source>Duration:</source>
+        <translation>Varaktighet:</translation>
+    </message>
+    <message>
+        <source>Distance:</source>
+        <translation>Distans:</translation>
+    </message>
+    <message>
+        <source>Description:</source>
+        <translation>Beskrivning:</translation>
+    </message>
+    <message>
+        <source>Speed max/⌀:</source>
+        <translation>Maxhastighet/⌀:</translation>
+    </message>
+    <message>
+        <source>Pace ⌀:</source>
+        <translation>Tempo ⌀:</translation>
+    </message>
+    <message>
+        <source>Heart rate min/max/⌀:</source>
+        <translation>Hjärtfrekvens min/max/⌀:</translation>
+    </message>
+    <message>
+        <source>Diagrams</source>
+        <translation>Diagram</translation>
+    </message>
+</context>
+<context>
+    <name>DiagramViewPage</name>
+    <message>
+        <source>Diagrams</source>
+        <translation>Diagram</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Start new workout</source>
+        <translation>Starta nytt träningspass</translation>
+    </message>
+    <message>
+        <source>Welcome to Laufhelden</source>
+        <translation>Välkommen till Laufhelden</translation>
+    </message>
+    <message>
+        <source>No earlier workouts</source>
+        <translation>Inga tidigare träningspass</translation>
+    </message>
+    <message>
+        <source>Workouts: </source>
+        <translation>Träningspass: </translation>
+    </message>
+    <message>
+        <source>of</source>
+        <translation>av</translation>
+    </message>
+    <message>
+        <source>Loading GPX files...</source>
+        <translation>Läser in GPX-filer...</translation>
+    </message>
+    <message>
+        <source>Remove workout</source>
+        <translation>Ta bort träningspasset</translation>
+    </message>
+    <message>
+        <source>Removing workout...</source>
+        <translation>Tar bort träningspass...</translation>
+    </message>
+    <message>
+        <source>Uncompleted workout found!</source>
+        <translation>Oavslutat träningspass hittat!</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Fortsätt</translation>
+    </message>
+</context>
+<context>
+    <name>PreRecordPage</name>
+    <message>
+        <source>Let&apos;s go!</source>
+        <translation>Starta!</translation>
+    </message>
+    <message>
+        <source>Workout:</source>
+        <translation>Träningspass:</translation>
+    </message>
+    <message>
+        <source>Use HRM device</source>
+        <translation>Använd hjärtfrekvensenhet</translation>
+    </message>
+    <message>
+        <source>Use heart rate monitor in this workout.</source>
+        <translation>Använd hjärtfrekvensmätare i detta träningspass</translation>
+    </message>
+    <message>
+        <source>Disable screen blanking</source>
+        <translation>Inaktivera skärmsläckning</translation>
+    </message>
+    <message>
+        <source>Disbale screen blanking when recording.</source>
+        <translation>Inaktivera skärmsläckning vid inspelning</translation>
+    </message>
+</context>
+<context>
+    <name>RecordPage</name>
+    <message>
+        <source>AMOLED mode</source>
+        <translation>AMOLED-läge</translation>
+    </message>
+    <message>
+        <source>LCD mode</source>
+        <translation>LCD-läge</translation>
+    </message>
+    <message>
+        <source>Night mode</source>
+        <translation>Nattläge</translation>
+    </message>
+    <message>
+        <source>Silica mode</source>
+        <translation>Silica-läge</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Hide Map</source>
+        <translation>Dölj karta</translation>
+    </message>
+    <message>
+        <source>Show Map</source>
+        <translation>Visa karta</translation>
+    </message>
+    <message>
+        <source>Disconnect HRM</source>
+        <translation>Koppla bort hjärtfrekvensmätare</translation>
+    </message>
+    <message>
+        <source>Reconnect HRM</source>
+        <translation>Återanslut hjärtfrekvensmätare</translation>
+    </message>
+    <message>
+        <source>Switch display mode</source>
+        <translation>Växla skärmläge</translation>
+    </message>
+    <message>
+        <source>hold button for: </source>
+        <translation>långtryck på knappen för: </translation>
+    </message>
+    <message>
+        <source>GPS accuracy:</source>
+        <translation>GPS-precision:</translation>
+    </message>
+    <message>
+        <source>No position</source>
+        <translation>Ingen position</translation>
+    </message>
+    <message>
+        <source>too low: </source>
+        <translation>för låg: </translation>
+    </message>
+    <message>
+        <source>Recording</source>
+        <translation>Spelar in</translation>
+    </message>
+    <message>
+        <source>Stopped</source>
+        <translation>Stoppad</translation>
+    </message>
+    <message>
+        <source>Paused</source>
+        <translation>Pausad</translation>
+    </message>
+    <message>
+        <source>Pace:</source>
+        <translation>Tempo:</translation>
+    </message>
+    <message>
+        <source>min/km</source>
+        <translation>min/km</translation>
+    </message>
+    <message>
+        <source>Speed:</source>
+        <translation>Hastighet:</translation>
+    </message>
+    <message>
+        <source>km/h</source>
+        <translation>km/t</translation>
+    </message>
+    <message>
+        <source>Distance:</source>
+        <translation>Distans:</translation>
+    </message>
+    <message>
+        <source>km</source>
+        <translation>km</translation>
+    </message>
+    <message>
+        <source>Heartrate:</source>
+        <translation>Hjärtfrekvens:</translation>
+    </message>
+    <message>
+        <source>bpm</source>
+        <translation>bpm</translation>
+    </message>
+    <message>
+        <source>Bat:</source>
+        <translation>Bat:</translation>
+    </message>
+    <message>
+        <source>Pace ⌀:</source>
+        <translation>Tempo ⌀:</translation>
+    </message>
+    <message>
+        <source>Speed ⌀:</source>
+        <translation>Hastighet ⌀:</translation>
+    </message>
+    <message>
+        <source>Duration:</source>
+        <translation>Varaktighet:</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Pausa</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Fortsätt</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Starta</translation>
+    </message>
+    <message>
+        <source>End</source>
+        <translation>Sluta</translation>
+    </message>
+</context>
+<context>
+    <name>SaveDialog</name>
+    <message>
+        <source>Save track</source>
+        <translation>Spara spårningen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>Beskrivning</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsMenu</name>
+    <message>
+        <source>General settings</source>
+        <translation>Generella inställningar</translation>
+    </message>
+    <message>
+        <source>Alarm thresholds</source>
+        <translation>Tröskelvärden för varning</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Heart rate device</source>
+        <translation>Hjärtfrekvensenhet</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>General settings</source>
+        <translation>Generella inställningar</translation>
+    </message>
+    <message>
+        <source>Keep record page in portrait mode.</source>
+        <translation>Behåll inspelningssidan i stående skärmläge.</translation>
+    </message>
+    <message>
+        <source>Write log file</source>
+        <translation>Skriv loggfil</translation>
+    </message>
+    <message>
+        <source>File: $HOME/Laufhelden/log.txt</source>
+        <translation>Fil: $HOME/Laufhelden/log.txt</translation>
+    </message>
+    <message>
+        <source>Record page portrait mode</source>
+        <translation>Inspelningssida i stående läge</translation>
+    </message>
+    <message>
+        <source>Voice language</source>
+        <translation>Röstspråk</translation>
+    </message>
+    <message>
+        <source>English male</source>
+        <translation>Engelska manlig</translation>
+    </message>
+    <message>
+        <source>German male</source>
+        <translation>Tyska kvinnlig</translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation>Test</translation>
+    </message>
+    <message>
+        <source>Show grid lines</source>
+        <translation>Visa stödlinjer</translation>
+    </message>
+    <message>
+        <source>Show grid lines on record page.</source>
+        <translation>Visa stödlinjer på inspelningssidan.</translation>
+    </message>
+</context>
+<context>
+    <name>SharedResources</name>
+    <message>
+        <source>Running</source>
+        <translation>Löpning</translation>
+    </message>
+    <message>
+        <source>Roadbike</source>
+        <translation>Landsvägscykel</translation>
+    </message>
+    <message>
+        <source>Mountainbike</source>
+        <translation>Mountainbike</translation>
+    </message>
+    <message>
+        <source>Walking</source>
+        <translation>Gång</translation>
+    </message>
+    <message>
+        <source>Inline skating</source>
+        <translation>Inlines</translation>
+    </message>
+</context>
+<context>
+    <name>ThresholdSettingsPage</name>
+    <message>
+        <source>Upper heart rate limit</source>
+        <translation>Övre hjärtfrekvensgräns</translation>
+    </message>
+    <message>
+        <source>Alarm if limit is exceeded.</source>
+        <translation>Varna om gränsen överskrids.</translation>
+    </message>
+    <message>
+        <source>bpm</source>
+        <translation>bpm</translation>
+    </message>
+    <message>
+        <source>Lower heart rate limit</source>
+        <translation>Undre hjärtfrekvensgräns</translation>
+    </message>
+    <message>
+        <source>Upper pace limit</source>
+        <translation>Övre tempogräns</translation>
+    </message>
+    <message>
+        <source>min/km</source>
+        <translation>min/km</translation>
+    </message>
+    <message>
+        <source>Lower pace limit</source>
+        <translation>Undre tempogräns</translation>
+    </message>
+    <message>
+        <source>Edit profile name</source>
+        <translation>Redigera profilnamn</translation>
+    </message>
+    <message>
+        <source>Only text, no special characters!</source>
+        <translation>Endast text, inga specialtecken!</translation>
+    </message>
+    <message>
+        <source>Threshold profile name</source>
+        <translation>Profilnamn för tröskelvärden</translation>
+    </message>
+    <message>
+        <source>Add profile</source>
+        <translation>Lägg till profil</translation>
+    </message>
+    <message>
+        <source>Rename profile</source>
+        <translation>Byt namn på profilen</translation>
+    </message>
+    <message>
+        <source>Remove profile</source>
+        <translation>Ta bort profilen</translation>
+    </message>
+    <message>
+        <source>Select thresholds profile</source>
+        <translation>Välj tröskelvärdesprofil</translation>
+    </message>
+    <message>
+        <source>Alarm if limit is undershot.</source>
+        <translation>Varna om gränsen understigs.</translation>
+    </message>
+    <message>
+        <source>Alarm thresholds</source>
+        <translation>Varningsnivåer</translation>
+    </message>
+</context>
+<context>
+    <name>Thresholds</name>
+    <message>
+        <source>Thresholds off</source>
+        <translation>Tröskelvärden av</translation>
+    </message>
+</context>
+</TS>
