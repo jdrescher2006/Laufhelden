40274352a56e115b0270d71d9fe872262e7b1640
Carlos   Gonzalez <carlosgonz@protonmail.com>
2017-10-10 07:41:35 -0400

    Update harbour-laufhelden-es.ts

--- translations/harbour-laufhelden-es.ts
+++ translations/harbour-laufhelden-es.ts
@@ -207,7 +207,7 @@
     </message>
     <message>
         <source>Select thresholds profile</source>
-        <translation type="unfinished">Seleccionar perfil de umbrales</translation>
+        <translation>Seleccionar perfil de umbrales</translation>
     </message>
 </context>
 <context>
@@ -454,7 +454,7 @@
     </message>
     <message>
         <source>No need to enter workout name on end of workout.</source>
-        <translation type="unfinished"></translation>
+        <translation>No es necesario entrar el nombre del ejercicio al final del entrenamiento<.</translation>
     </message>
 </context>
 <context>
