33137cc195e97ffa375d8174f19d02afaf78c5ee
Jens Drescher <pydj04@ubuntu-mate-1604>
2018-02-06 13:51:32 +0100

    Last changes for new Release.

--- qml/pages/AboutPage.qml
+++ qml/pages/AboutPage.qml
@@ -113,7 +113,7 @@ Page
                 anchors.horizontalCenter: parent.horizontalCenter
                 font.pixelSize: Theme.fontSizeExtraSmall
                 color: Theme.secondaryColor
-                text: qsTr("Date: ") + "05.02.2018";
+                text: qsTr("Date: ") + "06.02.2018";
             }
             Label
             {
--- translations/harbour-laufhelden-de.ts
+++ translations/harbour-laufhelden-de.ts
@@ -1086,21 +1086,21 @@
     <name>StravaComments</name>
     <message>
         <source>Comments</source>
-        <translation type="unfinished"></translation>
+        <translation>Kommentare</translation>
     </message>
 </context>
 <context>
     <name>StravaKudos</name>
     <message>
         <source>Kudos</source>
-        <translation type="unfinished"></translation>
+        <translation>Kudos</translation>
     </message>
 </context>
 <context>
     <name>StravaSegments</name>
     <message>
         <source>Segments</source>
-        <translation type="unfinished"></translation>
+        <translation>Segmente</translation>
     </message>
 </context>
 <context>
